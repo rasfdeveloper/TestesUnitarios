b87b13f76fd8ac331a07ede38920fa3466188dd8
Etica <Etica@NTB-CCE.etica.local>
2018-07-30 09:38:43 -0300

    Implementando spy, baixando dependencias do powermock

--- src/test/java/br/ce/wcaquino/servicos/CalculadoraMockTest.java
+++ src/test/java/br/ce/wcaquino/servicos/CalculadoraMockTest.java
@@ -2,11 +2,46 @@ package br.ce.wcaquino.servicos;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 
 public class CalculadoraMockTest {
+	
+	@Mock
+	private Calculadora calcMock;
+	
+	@Mock
+	private EmailService email;
+	
+	@Spy
+	private Calculadora calcSpy;
+	
+	@Before
+	public void setup(){
+		MockitoAnnotations.initMocks(this);
+	}
+	
+	@Test
+	public void devoMostrarDiferencaEntreMockSpy(){
+		Mockito.when(calcMock.somar(1, 2)).thenReturn(5);
+//		Mockito.when(calcSpy.somar(1, 2)).thenReturn(5);
+		Mockito.doReturn(5).when(calcSpy).somar(1,2); //Mestre yoda
+		
+		Mockito.doNothing().when(calcSpy).imprime();
+		
+		System.out.println("Mock:" + calcMock.somar(1, 2));
+		System.out.println("Spy:" + calcSpy.somar(1, 2));
+		System.out.println("Mock");
+		calcMock.imprime();
+		System.out.println("Spy");
+		calcSpy.imprime();
+	}
+	
 	
 	@Test
 	public void teste(){
@@ -16,7 +51,8 @@ public class CalculadoraMockTest {
 		Mockito.when(calc.somar(argCapt.capture(), argCapt.capture())).thenReturn(5);
 		
 		assertEquals(5, calc.somar(123124,21412412));
-		System.out.println(argCapt.getAllValues());
+//		System.out.println(argCapt.getAllValues());
+		
 	}
 
 }
